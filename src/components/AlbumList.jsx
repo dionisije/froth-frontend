e0b6758ef270d0f380568c967af9bdf0688f3b2d
dionisije <dick@marjanovic.co.uk>
2022-04-28 18:58:55 +0100

    FF-8 - Display all albums as a card grid (#4)
    * Separate out album series
    
    * Add unit test
    
    Co-authored-by: Dick Marjanovic <dick.marjanovic@accenture.com>

--- src/components/AlbumList.jsx
+++ src/components/AlbumList.jsx
@@ -2,7 +2,9 @@ import React, {useEffect, useState} from 'react';
 import FrothDataService from '../services/froth';
 
 const AlbumList = () => {
-    const [albumTitles, setAlbumTitles] = useState([]);
+    const [originalTitles, setOriginalTitles] = useState([]);
+    const [classicTitles, setClassicTitles] = useState([]);
+    const [streamingTitles, setStreamingTitles] = useState([]);
 
     useEffect(() => {
         retrieveAlbumTitles()
@@ -12,7 +14,9 @@ const AlbumList = () => {
         FrothDataService.getAll()
             .then(response => {
                 console.log(response.data);
-                setAlbumTitles(response.data);
+                setOriginalTitles(response.data.originalSeries);
+                setClassicTitles(response.data.classicSeries);
+                setStreamingTitles(response.data.streamingSeries);
             })
             .catch(err => {
                 console.error('FDS returned an error:', err);
@@ -22,11 +26,30 @@ const AlbumList = () => {
     return (
         <>
             <p>List of albums here...</p>
-            <ol>
-                {albumTitles ? (
-                    albumTitles.map(album => <li key={album._id}>{album.Album}</li>)
-                ) : null}
-            </ol>
+            {originalTitles ? (
+                <>
+                    <h3>Original series...</h3>
+                    <ol>
+                        {originalTitles.map(album => <li key={album._id}>{album.Name} ({album.Catalogue})</li>)}
+                    </ol>
+                </>
+            ) : null}
+            {classicTitles ? (
+                <>
+                    <h3>Classic series...</h3>
+                    <ol>
+                        {classicTitles.map(album => <li key={album._id}>{album.Name} ({album.Catalogue})</li>)}
+                    </ol>
+                </>
+            ) : null}
+            {streamingTitles ? (
+                <>
+                    <h3>Streaming series...</h3>
+                    <ol>
+                        {streamingTitles.map(album => <li key={album._id}>{album.Name} ({album.Catalogue})</li>)}
+                    </ol>
+                </>
+            ) : null}
         </>
     );
 };
