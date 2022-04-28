d6f5529bffdb015697ee3392c4cc4523624a3fb3
Dick Marjanovic <dick.marjanovic@accenture.com>
2022-04-28 12:35:52 +0100

    Add test for list

--- src/routes/Albums/Albums.jsx
+++ src/routes/Albums/Albums.jsx
@@ -1,2 +1,11 @@
-const Albums = () => (<main className="container"><h3>Albums</h3></main>);
+import React from 'react';
+import AlbumList from '../../components/AlbumList';
+
+const Albums = () => (
+    <main className="container">
+        <h3>Albums</h3>
+        <AlbumList />
+    </main>
+);
+
 export default Albums;
