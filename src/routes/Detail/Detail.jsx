8273886b4defa4feb860c758e511c9c459f60829
Dick Marjanovic <dick.marjanovic@accenture.com>
2022-05-04 15:13:14 +0100

    Display incomming artist and track name

--- src/routes/Detail/Detail.jsx
+++ src/routes/Detail/Detail.jsx
@@ -16,6 +16,7 @@ const Detail = () => {
     const retrieveAlbumDetail = id => {
         FrothDataService.getAlbum(id)
         .then(response => {
+            console.log(response.data);
             setAlbumTracks(response.data);
         })
             .catch(err => {
@@ -25,27 +26,29 @@ const Detail = () => {
 
     return (
         <main className='container col-xxl-8 px-4 py-5'>
-            <div className='row flex-lg-row-reverse align-items-center g-5 py-5'>
+            {albumTracks.length > 0 && (
                 <div className='row flex-lg-row-reverse align-items-center g-5 py-5'>
-                    <div className='col-10 col-sm-8 col-lg-6'>
-                        <h2>Detail</h2>
-                        <p>Album ID: {albumId}</p>
-                        <ol>
-                            {albumTracks.map(track => <li key={track}>{track}</li>)}
-                        </ol>
-                    </div>
-                    <div className='col-lg-6'>
-                        <img
-                            src={`${imageUrl}`}
-                            className='d-block mx-lg-auto img-fluid'
-                            alt={`Album cover for ${albumId}`}
-                            width={500}
-                            height={500}
-                            loading='lazy'
-                        />
+                    <div className='row flex-lg-row-reverse align-items-center g-5 py-5'>
+                        <div className='col-10 col-sm-8 col-lg-6'>
+                            <h2>{albumTracks[0].Album}</h2>
+                            <h6>{albumTracks[0].Catalogue}</h6>
+                            <ol>
+                                {albumTracks.map(track => <li key={track._id}>{track.Artist} - {track.Name}</li>)}
+                            </ol>
+                        </div>
+                        <div className='col-lg-6'>
+                            <img
+                                src={`${imageUrl}`}
+                                className='d-block mx-lg-auto img-fluid'
+                                alt={`Album cover for ${albumId}`}
+                                width={500}
+                                height={500}
+                                loading='lazy'
+                            />
+                        </div>
                     </div>
                 </div>
-            </div>
+            )}
         </main>
     );
 };
