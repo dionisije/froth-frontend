bfb4bd3d6696a71598b1810f87810d186220fc66
Dick Marjanovic <dick.marjanovic@accenture.com>
2022-05-09 14:44:32 +0100

    Add search field

--- src/routes/Search/Search.jsx
+++ src/routes/Search/Search.jsx
@@ -1,9 +1,40 @@
-import React from 'react';
+import React, {useState} from 'react';
 
-const Search = () => (
-    <main className="container">
-        <h2>Search</h2>
-    </main>
-);
+const Search = () => {
+    const [searchText, setSearchText] = useState('');
+
+    const onChangeSearchText = e => {
+        setSearchText(e.target.value);
+    }
+    const searchForText = () => {
+        console.log(searchText);
+    };
+
+    return (
+        <main className="container">
+            <h2>Search</h2>
+            <form>
+                <div className='input-group mb-3'>
+                    <input
+                        className='form-control'
+                        type='search'
+                        placeholder='Search for artist, track or album'
+                        aria-label='Search for artist, track or album'
+                        aria-describedby='search-button'
+                        onChange={onChangeSearchText}
+                    />
+                    <button
+                        className='btn btn-outline-secondary'
+                        type='button'
+                        id='search-button'
+                        onClick={searchForText}
+                    >
+                        Search
+                    </button>
+                </div>
+            </form>
+        </main>
+    );
+};
 
 export default Search;
