163d99d274417853593eadc267b729eb5f1ff5fb
dionisije <dick@marjanovic.co.uk>
2022-05-09 18:32:39 +0100

    FF-20 - Incorporate Spotify playlists to app pages (#13)
    * Addd streaming series as Spotify playlistlinks
    
    * Insert dynamic Spotify iframe
    
    Co-authored-by: Dick Marjanovic <dick.marjanovic@accenture.com>

--- src/routes/Detail/Detail.jsx
+++ src/routes/Detail/Detail.jsx
@@ -1,38 +1,45 @@
 import React, {useEffect, useState} from 'react';
 import { useParams } from 'react-router-dom';
 import TrackTable from '../../components/TrackTable/TrackTable';
+import StreamTable from '../../components/StreamTable/StreamTable';
 import FrothDataService from '../../services/froth';
 
 
 const Detail = () => {
     let { albumId }  = useParams();
     let imageUrl = './froth-frontend/public/logo512.png';
-    const regex = /^DVD((CD)|(RM))R?\d\d?$/;
+    const isStreamingRegex = /DVDPL\d+/;
+    const isStreamingTitle = albumId && isStreamingRegex.test(albumId);
+    const isCatalogueRegex = /^DVD((CD)|(CDR)|(PL))?\d\d?$/;
 
-    if (albumId && regex.test(albumId)) {
+    if (albumId && isCatalogueRegex.test(albumId)) {
         imageUrl = `http://frothmusic.co.uk/frothmusic/images/covers/${albumId.toLowerCase()}.jpg`;
     }
 
     const [albumTracks, setAlbumTracks] = useState([]);
 
     useEffect(() => {
-        retrieveAlbumDetail(albumId);
-    }, [albumId]);
+        const retrieveAlbumDetail = id => {
+            if (!isStreamingTitle) {
+                FrothDataService.getAlbum(id)
+                    .then(response => {
+                        setAlbumTracks(response.data);
+                    })
+                    .catch(err => {
+                        console.error('FDS returned an error from Detail:', err);
+                    });
+            } else {
+                setAlbumTracks([]);
+            }
+        };
 
-    const retrieveAlbumDetail = id => {
-        FrothDataService.getAlbum(id)
-        .then(response => {
-            console.log(response.data);
-            setAlbumTracks(response.data);
-        })
-            .catch(err => {
-                console.error('FDS returned an error:', err);
-            });
-    };
+        retrieveAlbumDetail(albumId);
+    }, [albumId, isStreamingTitle]);
+
 
     return (
         <main className='container' data-testid='detail'>
-            {albumTracks.length > 0 && (
+            {isStreamingTitle ? (
                 <>
                     <div className='row align-items-center'>
                         <div className='col-md mx-5 my-2'>
@@ -46,13 +53,34 @@ const Detail = () => {
                             />
                         </div>
                         <div className='col-md mx-5 my-2'>
-                            <h1>{albumTracks[0][0].Album}</h1>
+                            <h1>{albumId}</h1>
                         </div>
-                    </div>
-                    <div className='row align-items-center mx-5 my-2'>
-                        <TrackTable data={albumTracks} />
+                        <StreamTable data={albumId} />
                     </div>
                 </>
+            ) : (
+                (albumTracks && albumTracks.length > 0) && (
+                    <>
+                        <div className='row align-items-center'>
+                            <div className='col-md mx-5 my-2'>
+                                <img
+                                    src={`${imageUrl}`}
+                                    className='d-block mx-md-auto img-fluid'
+                                    alt={`Album cover for ${albumId}`}
+                                    width={400}
+                                    height={400}
+                                    loading='lazy'
+                                />
+                            </div>
+                            <div className='col-md mx-5 my-2'>
+                                <h1>{albumTracks[0][0].Album}</h1>
+                            </div>
+                        </div>
+                        <div className='row align-items-center mx-5 my-2'>
+                            <TrackTable data={albumTracks} />
+                        </div>
+                    </>
+                )
             )}
         </main>
     );
